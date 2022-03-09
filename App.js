7ed61ac5b168e3373107d5a3217e581de1768b6b
jihami <w2027@e-mirim.hs.kr>
2022-03-09 19:16:53 +0900

    layout(flex)

--- App.js
+++ App.js
@@ -3,10 +3,15 @@ import { View } from 'react-native';
 
 export default function App() {
   return ( 
-    <View style={{ flexDirection:"row"}}>
-      <View style={{width:100, height:100, backgroundColor:"tomato"}}></View>
-      <View style={{width:100, height:100, backgroundColor:"teal"}}></View>
-      <View style={{width:100, height:100, backgroundColor:"orange"}}></View>
-    </View>
+    // <View style={{flex:1}}> // 세로
+    //   <View style={{flex:1,backgroundColor:"tomato"}}></View>
+    //   <View style={{flex:6,backgroundColor:"teal"}}></View>
+    //   <View style={{flex:1,backgroundColor:"orange"}}></View>
+    // </View>
+    <View style={{flex:1, flexDirection:"row"}}> //가로
+    <View style={{flex:1,backgroundColor:"tomato"}}></View>
+    <View style={{flex:6,backgroundColor:"teal"}}></View>
+    <View style={{flex:1,backgroundColor:"orange"}}></View>
+  </View>
   );
 }
