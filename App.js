72fd130ccfbe4f3dce4d81d86c796a470e7962c0
jihami <w2027@e-mirim.hs.kr>
2022-03-09 19:10:05 +0900

    row

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { View } from 'react-native';
 
 export default function App() {
   return ( 
-    <View>
+    <View style={{ flexDirection:"row"}}>
       <View style={{width:100, height:100, backgroundColor:"tomato"}}></View>
       <View style={{width:100, height:100, backgroundColor:"teal"}}></View>
       <View style={{width:100, height:100, backgroundColor:"orange"}}></View>
