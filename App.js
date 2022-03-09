41b159aeef0e372f9610ca08ebeb88debcac7992
jihami <w2027@e-mirim.hs.kr>
2022-03-09 20:30:38 +0900

    2.5 Styles

--- App.js
+++ App.js
@@ -1,18 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
+import { View, Text, StyleSheet} from 'react-native';
 
 export default function App() {
-  return ( 
-    // <View style={{flex:1}}> // 세로
-    //   <View style={{flex:1,backgroundColor:"tomato"}}></View>
-    //   <View style={{flex:6,backgroundColor:"teal"}}></View>
-    //   <View style={{flex:1,backgroundColor:"orange"}}></View>
-    // </View>
-    // 가로
-    <View style={{flex:1, flexDirection:"row"}}> 
-    <View style={{flex:1,backgroundColor:"tomato"}}></View>
-    <View style={{flex:6,backgroundColor:"teal"}}></View>
-    <View style={{flex:1,backgroundColor:"orange"}}></View>
+  return  (
+  <View style={styles.container}>
+    <StatusBar style='light'/>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>서울</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.desciption}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.desciption}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.desciption}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.desciption}>Sunny</Text>
+        </View>
+      </View>
   </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:"tomato"
+  },
+  city:{
+    flex: 1.2,
+    justifyContent:"center",
+    alignItems:"center",
+  },
+  cityName:{
+    fontSize:68,
+    fontWeight:"500"
+  },
+  weather:{
+    flex:3,
+    // backgroundColor: "teal",
+  },
+  day:{
+    flex:1,
+    alignItems:"center",
+  },
+  temp:{
+    marginTop:50,
+    fontSize:178,
+  },
+  desciption:{
+    marginTop:-30,
+    fontSize:60,
+  },
+});
